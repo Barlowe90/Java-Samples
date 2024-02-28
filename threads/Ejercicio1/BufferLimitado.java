c95514ebabee355b7e253e5c1ab8776f9deecf69
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2024-02-28 17:41:16 +0100

    using monitor

--- threads/Ejercicio1/BufferLimitado.java
+++ threads/Ejercicio1/BufferLimitado.java
@@ -1,56 +1,45 @@
 package threads.Ejercicio1;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class BufferLimitado {
 
     private char buffer[];
-    private int ocupados, cola, frente;
-    private Lock lock = new ReentrantLock();
-    private Condition productor = lock.newCondition();
-    private Condition consumidor = lock.newCondition();
+    private int cont;
+    private Object lock = new Object();
 
     public BufferLimitado(int capacidad) {
         buffer = new char[capacidad];
-        this.ocupados = this.cola = this.frente = 0;
+        this.cont = 0;
     }
 
     public void poner(char c) {
-        lock.lock();
-        try {
-            while (ocupados >= buffer.length) {
-                productor.await();
+        synchronized (lock) {
+            while (cont == buffer.length) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            buffer[frente] = c;
-            cola = frente;
-            frente = (frente + 1) % buffer.length;
-            ocupados++;
-            consumidor.signal();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
+            buffer[cont] = c;
+            cont++;
+            lock.notifyAll();
         }
     }
 
     public char sacar() {
-        lock.lock();
-        char c = 0;
-        try {
-            while (ocupados <= 0) {
-                consumidor.await();
+        synchronized (lock) {
+            char c = 0;
+            try {
+                while (cont == 0) {
+                    lock.wait();
+                }
+                c = buffer[cont];
+                cont--;
+                lock.notifyAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            c = buffer[cola];
-            ocupados--;
-            cola = (cola - 1 + buffer.length) % buffer.length;
-            productor.signal();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
+            return c;
         }
-        return c;
     }
 }
